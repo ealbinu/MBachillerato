258c15021957e40d90b21d347f99007590435d2d
Albin Rdz <ealbinu@gmail.com>
2021-11-29 19:49:26 -0600

    test 3

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-npm run build && git subtree push --prefix dist origin gh-pages
+npm run build && git commit -m "Update" && git push origin master && git subtree push --prefix dist origin gh-pages
 
 #https://gist.github.com/cobyism/4730490
