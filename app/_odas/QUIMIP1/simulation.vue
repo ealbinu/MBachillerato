dfd34d62c0826404e45ebff84a98a2ca9341b84a
Albin Rodriguez <ealbinu@gmail.com>
2022-04-26 00:49:41 -0500

    Updates

---
+++ app/_odas/QUIMIP1/simulation.vue
@@ -0,0 +1,14 @@
+<template>
+<div @click="suma++" class="sumando">QUIMISIMULATION {{suma}}</div>
+</template>
+<script setup>
+import {ref, inject} from 'vue'
+const suma = ref(1)
+const Activity = inject('activityFile')
+console.log(Activity)
+</script>
+<style>
+.sumando{
+    background: #ffdd00;
+}
+</style>
